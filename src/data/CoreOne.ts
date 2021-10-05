e045db2dac437e77f354ed721c3685d7698e95ed
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-06 00:11:30 +0100

    coreOne data array

---
+++ src/data/CoreOne.ts
@@ -0,0 +1,22 @@
+import { nanoid } from 'nanoid';
+import secureIcon from '../assets/corefeatures1/secureIcon.svg'
+import smartIcon from '../assets/corefeatures1/smartIcon.svg'
+
+const coreOne = [
+    {
+        id: nanoid(),
+        img: smartIcon,
+        title: 'Smart Features',
+        text:
+            'Get your blood tests delivered at let home collect sample from the victory of the managements. your blood tests.',
+    },
+    {
+        id: nanoid(),
+        img: secureIcon,
+        title: 'Secure Contents',
+        text:
+            'Get your blood tests delivered at let home collect sample from the victory of the managements. your blood tests.',
+    },
+];
+
+export default coreOne
