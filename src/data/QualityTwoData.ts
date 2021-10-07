d19d435b800fce59d447988e73d2eab64b4414ef
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-07 14:42:38 +0100

    quality feature two data array

---
+++ src/data/QualityTwoData.ts
@@ -0,0 +1,54 @@
+import { nanoid } from 'nanoid';
+import smartfeaturesicon from '../assets/qualityfeatures2/smartfeaturesicon.svg'
+import fastperformance from '../assets/qualityfeatures2/smartperformance.svg'
+import unlimitedcontent from '../assets/qualityfeatures2/unlimitedcontent.svg'
+import unlimitedcustomisation from '../assets/qualityfeatures2/ultimatecustomisation.svg'
+import boostproductivity from '../assets/qualityfeatures2/boostproductivity.svg'
+import customersupport from '../assets/qualityfeatures2/customersupport.svg'
+
+const QualityTwoData = [
+    {
+        id: nanoid(),
+        img: smartfeaturesicon,
+        title: 'Smart Features',
+        text:
+            'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+    {
+        id: nanoid(),
+        img: fastperformance,
+        title: 'Fast Performance',
+        text:
+            'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+    {
+        id: nanoid(),
+        img: unlimitedcontent,
+        title: 'Unlimited Content',
+        text:
+            'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+    {
+        id: nanoid(),
+        img: unlimitedcustomisation,
+        title: 'Unlimited Customization',
+        text:
+            'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+    {
+        id: nanoid(),
+        img: boostproductivity,
+        title: 'Boost Productivity',
+        text:
+            'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+    {
+        id: nanoid(),
+        img: customersupport,
+        title: 'Customer Support',
+        text:
+            'Get your blood tests delivered at let home collect sample from the victory of the managements.',
+    },
+];
+
+export default QualityTwoData;
