c6496b82441ced8328d77f8be01f2d605f591ee3
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-08 11:40:13 +0100

    added the data array for functionOne

---
+++ src/data/FunctionOneData.ts
@@ -0,0 +1,35 @@
+import { nanoid } from "nanoid"
+
+const FunctionOneData = [
+    {
+        id: nanoid(),
+        number: '01',
+        title: 'Set disbursement Instructions',
+        text:
+            'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    },
+    {
+        id: nanoid(),
+        number: '02',
+        title: 'Assembly retrieves funds from your account',
+        text:
+            'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    },
+    {
+        id: nanoid(),
+        number: '03',
+        title: 'Assembly initiates disbursement',
+        text:
+            'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    },
+    {
+        id: nanoid(),
+        number: '04',
+        title: 'Customer receives funds payment',
+        text:
+            'Get your blood tests delivered at home collect a sample from the your blood tests.',
+    },
+];
+
+
+export default FunctionOneData
