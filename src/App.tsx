d5dec89c91671733148553730c0e67aa04a49ddc
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-08 11:42:20 +0100

    imports & exports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { QualityFeatureOne, Header, CoreFeaturesOne, QualityFeatureTwo, CoreFeaturesTwo } from './sections'
+import { QualityFeatureOne, Header, CoreFeaturesOne, QualityFeatureTwo, CoreFeaturesTwo, FunctionOne } from './sections'
 
 
 const App = () => {
@@ -9,6 +9,7 @@ const App = () => {
       <CoreFeaturesOne />
       <QualityFeatureTwo />
       <CoreFeaturesTwo />
+      <FunctionOne />
       <section style={{height: '100vh', backgroundColor: 'tomato'}}></section>
       {/* <section></section> */}
       {/* <section></section> */}
