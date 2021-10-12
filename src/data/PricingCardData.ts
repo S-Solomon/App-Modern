1b9bc9e508ca0df696e6192327d2a7bd04a8e2a0
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-12 05:59:21 +0100

    pricingcard data array

---
+++ src/data/PricingCardData.ts
@@ -0,0 +1,19 @@
+import { nanoid } from "nanoid";
+import greenTick from '../assets/function2/greenTick.svg'
+import close from '../assets/function2/close.svg'
+
+
+const PricingCardData = [
+    {
+        id: nanoid(),
+        img1: greenTick,
+        img2: close,
+        p1: 'Ultimate access to all course, exercises and assessments',
+        p2: 'Free access for all kind of exercise corrections with downloads.',
+        p3: 'Total assessment corrections with free download access system',
+        p4: 'Unlimited download of courses on the mobile app contents',
+        p5: 'Download and print courses and exercises in PDF',
+    },
+    
+]
+export default PricingCardData
