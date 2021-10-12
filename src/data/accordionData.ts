7d0fb6144c34e6b719195b99e7477915d001c798
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-12 07:45:38 +0100

    accordion data array

--- src/data/accordionData.ts
+++ src/data/accordionData.ts
@@ -3,27 +3,27 @@ import { nanoid } from "nanoid";
 const accordionData = [
     {
         id: nanoid(),
-        question: 'Question 1',
+        question: ' How to contact with riders emergency ?',
         answer:
-            'Lorem ipsum cing elit. Consectetur minima deserunt, pe sit magnam? Voluptatem rem quibusdam consectetur praesentium vel, perferendis tempora et!',
+            'Get your website ads tests delivered at let collect sample from the victory of the managements that supplies best design system which guidelines ever with multiple features.',
     },
     {
         id: nanoid(),
-        question: 'Question 2',
+        question: 'App installation failed, how to update system information?',
         answer:
-            'Lorem ipsum dolotetur minima deserunt, eligendi quia ut numquam odio. Vero distinctio saepe sit magnam? Voluptatem rem quibusdam consectetur praesentium vel, perferendis tempora et!',
+            'Rhino skin maybe thick but it can be quite sensitive to sunburns and insect bites which is why they like wallow so much – when the mud dries it acts as protection from the sunburns and insects.',
     },
     {
         id: nanoid(),
-        question: 'Question 3',
+        question: 'Website response taking time, how to improve?',
         answer:
-            'Lorem ipsum dolor sit elit. Consectetur minima deserunt, eligendi quia ut numquam odio. Vero distinctio saepe sit magnam? Voluptatem rem quibusdam consectetur praesentium vel, perferendis tempora et!',
-    },
+            'Use asynchronous loading for CSS and JavaScript files Once you’ve minified and combined some of your files, you can also optimize the way that they load on your pages.' 
+    },        
     {
         id: nanoid(),
-        question: 'Question 4',
+        question: 'New update fixed all bug and issues',
         answer:
-            'Lorem ipsum dolor sit amet consectetur, . Consectetur minima deserunt, eligendi quia ut numquam odio. Vero distinctio saepe sit magnam? Voluptatem rem quibusdam consectetur praesentium vel, perferendis tempora et!',
+            'If you’re looking to hunt a unicorn, but don’t know where to begin, try Lake Superior State University in Sault Ste. Marie, Michigan. Since 1971, the university has issued permits to unicorn questers.',
     },
 ];
 
