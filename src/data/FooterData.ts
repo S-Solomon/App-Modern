391a9cd4d92b4520d9bfdc24e2d2ac47ee92049f
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-12 09:29:55 +0100

    footer data changes

---
+++ src/data/FooterData.ts
@@ -0,0 +1,43 @@
+import { nanoid } from "nanoid"
+
+
+const FooterData = [
+    {
+        id: nanoid(),
+        title: 'About Us',
+        link1: 'Support Center',
+        link2: 'Customer Support',
+        link3: 'About Us',
+        link4: 'Copyright',
+        link5: 'Popular Campaign',
+    },
+    {
+        id: nanoid(),
+        title: 'Our Information',
+        link1: 'Return Policy',
+        link2: 'Privacy Policy',
+        link3: 'Terms & Conditions',
+        link4: 'Site Map',
+        link5: 'Store Hours',
+    },
+    {
+        id: nanoid(),
+        title: 'My Account',
+        link1: 'Press inquiries',
+        link2: 'Social media',
+        link3: 'directories',
+        link4: 'Images & B-roll',
+        link5: 'Permissions',
+    },
+    {
+        id: nanoid(),
+        title: 'Policy',
+        link1: 'Application security',
+        link2: 'Software principles',
+        link3: 'Unwanted software policy',
+        link4: 'Responsible supply chain',
+        link5: 'Carpe diem'
+    },
+];
+
+export default FooterData
