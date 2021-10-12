8368b7fb40a151cb95f8ee13466b06c4a53e89be
Samuel Solomon <mrsamuelsolomon@gmail.com>
2021-10-12 06:32:00 +0100

    accordion data

---
+++ src/data/accordionData.ts
@@ -0,0 +1,30 @@
+import { nanoid } from "nanoid";
+
+const accordionData = [
+    {
+        id: nanoid(),
+        question: 'Question 1',
+        answer:
+            'Lorem ipsum cing elit. Consectetur minima deserunt, pe sit magnam? Voluptatem rem quibusdam consectetur praesentium vel, perferendis tempora et!',
+    },
+    {
+        id: nanoid(),
+        question: 'Question 2',
+        answer:
+            'Lorem ipsum dolotetur minima deserunt, eligendi quia ut numquam odio. Vero distinctio saepe sit magnam? Voluptatem rem quibusdam consectetur praesentium vel, perferendis tempora et!',
+    },
+    {
+        id: nanoid(),
+        question: 'Question 3',
+        answer:
+            'Lorem ipsum dolor sit elit. Consectetur minima deserunt, eligendi quia ut numquam odio. Vero distinctio saepe sit magnam? Voluptatem rem quibusdam consectetur praesentium vel, perferendis tempora et!',
+    },
+    {
+        id: nanoid(),
+        question: 'Question 4',
+        answer:
+            'Lorem ipsum dolor sit amet consectetur, . Consectetur minima deserunt, eligendi quia ut numquam odio. Vero distinctio saepe sit magnam? Voluptatem rem quibusdam consectetur praesentium vel, perferendis tempora et!',
+    },
+];
+
+export default accordionData;
